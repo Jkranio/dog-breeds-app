25eda88c1b81cf13795b8591532135d2176fe936
ext_jcartolorza <juancarlos@kranio.io>
2024-05-16 01:23:19 -0400

    Update main workflow

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const axios = require('axios')
-require('dotenv').config()
+require('dotenv').config();
 
 const app = express()
 const apiKey = process.env.DOG_API_KEY
